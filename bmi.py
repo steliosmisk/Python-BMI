6627771040045b3b9edd9bc608c2a9eddba8ce5c
Stelios Miskedakis <123480703+steliosmisk@users.noreply.github.com>
2023-03-04 08:50:25 +0200

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,24 +1,25 @@
 def bmi(weight, height):
-        bmi = weight / (height/100)**2
-        if bmi <= 18.5:  
-            print("You are underweight.")  
-        elif bmi <= 24.9:  
-            print("You are healthy.")  
-        elif bmi <= 29.9:  
-            print("You are over weight.")  
-        else:  
-            print("You are obese.")  
-        return bmi
+    bmi = weight / (height/100)**2
+    if bmi <= 18.5:
+        print("You are underweight.")
+    elif bmi <= 24.9:
+        print("You are healthy.")
+    elif bmi <= 29.9:
+        print("You are overweight.")
+    else:
+        print("You are obese.")
+    return bmi
 
 print("--Welcome to BMI Calculator--")
-print("[1] Start")
+print("[1] Calculate BMI")
 print("[2] Exit")
 
 user_choice = int(input("Enter (1/2): "))
+
 while user_choice == 1:
     try:
         weight = int(input("[Kg] Enter your weight: "))
-        height = int(input("[Cm]Enter your height: "))
+        height = int(input("[Cm] Enter your height: "))
         print("BMI Results:", bmi(weight, height))
         break
     except:
